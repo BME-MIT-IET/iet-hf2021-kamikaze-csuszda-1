3d2b9e10c647dd2d53e1356e61eca1d7c5e5871c
Peti <abordanpeter@gmail.com>
2021-05-06 18:16:29 +0200

    Unit tests for automorphic_numbers.js

---
+++ code/mathematical_algorithms/test/automorphic_numbers.spec.js
@@ -0,0 +1,33 @@
+const automorphic = require('../src/automorphic_numbers/automorphic_numbers');
+
+test('Returns true for 5', () => {
+    expect(automorphic(5)).toBe(true);
+});
+
+test('Returns false for 67', () => {
+    expect(automorphic(67)).toBe(false);
+});
+
+test('Returns true for 0', () => {
+    expect(automorphic(0)).toBe(true);
+});
+
+test('Returns false for -1', () => {
+    expect(automorphic(-1)).toBe(false);
+});
+
+test('Returns false for automorphic looking double 2.1', () => {
+    expect(automorphic(2.1)).toBe(false);
+});
+
+test('Returns false for automorphic looking double 1.0', () => {
+    expect(automorphic(1.0)).toBe(false);
+});
+
+test('Returns false non automorphic looking double 2.0', () => {
+    expect(automorphic(2.0)).toBe(false);
+});
+
+test('Returns false for for automorphic looking double 0.0', () => {
+    expect(automorphic(0.0)).toBe(false);
+});
