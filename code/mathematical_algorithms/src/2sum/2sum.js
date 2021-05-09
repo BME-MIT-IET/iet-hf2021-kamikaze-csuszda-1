fbd6ca26ea563c253403d31dc224985b83af48d2
Peti <abordanpeter@gmail.com>
2021-05-09 19:33:16 +0200

    Fixed the implementation of get2sum

--- code/mathematical_algorithms/src/2sum/2sum.js
+++ code/mathematical_algorithms/src/2sum/2sum.js
@@ -1,6 +1,14 @@
 /*Part of cosmos by OpenGenus Foundation*/
-function get2sum(a, b) {
-    return a + b;
+function get2sum(nums, target) {
+    let dic = {};
+    for (let i = 0; i < nums.length; i++) {
+        if (target - nums[i] in dic) {
+            return [dic[target - nums[i]], i];
+        }
+        dic[nums[i]] = i;
+    }
+    // If the sum is not found return an empty array
+    return [];
 }
 
 console.log(get2sum(2, 3));
