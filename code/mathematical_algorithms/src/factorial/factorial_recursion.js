75c2b2312c5ac87891e78971c9dc07f8b13b326a
Peti <abordanpeter@gmail.com>
2021-05-11 21:33:59 +0200

    Fixed the stackoverflow error in the recursive factorial function (JS)

--- code/mathematical_algorithms/src/factorial/factorial_recursion.js
+++ code/mathematical_algorithms/src/factorial/factorial_recursion.js
@@ -1,7 +1,15 @@
 /* Part of Cosmos by OpenGenus Foundation */
 function factorial(n) {
-  return n === 0 ? 1 : n * factorial(n - 1);
+    if (n < 0) return undefined;
+    if (n % 1 != 0) return undefined;
+    return factorialHelper(n);
+}
+
+function factorialHelper(n) {
+    return n === 0 ? 1 : n * factorial(n - 1);
 }
 
 const num = 3;
 console.log(factorial(num));
+
+module.exports = factorial;
